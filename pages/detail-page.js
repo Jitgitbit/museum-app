50c9ba52c575c3e55b93f1170207b429e42ad656
Jitgitbit <rowowolf@gmail.com>
2020-01-09 15:36:28 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,29 @@
 function submitComment() {
     console.log("Hello!");
+
+    const inputField = document.getElementById('name');
+    console.log(inputField);
+
+    const name = inputField.value;
+    console.log(name);
+
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+    console.log(msg);
+
+    const comment = document.createElement('section');
+    console.log(comment);
+
+    const h3 = document.createElement('h3');
+    console.log(h3);
+
+    const p = document.createElement('p');
+    console.log(p);
+    
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
   };
